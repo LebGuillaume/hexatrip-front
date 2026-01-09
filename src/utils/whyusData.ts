efa31b7919247a91bc9922db05bd8fb2551fd115
Guillaume Lebegue <lebegue.guillaume26@gmail.com>
2026-01-09 20:47:27 +0100

    fix nom image

--- src/utils/whyusData.ts
+++ src/utils/whyusData.ts
@@ -1,10 +1,10 @@
-import credit from "../assets/images/whyus/credit-card-pay-visa-funds-svgrepo-com.svg";
-import cards from "../assets/images/whyus/papers-stack-svgrepo-com.svg";
-import man from "../assets/images/whyus/man-talking-avatar-svgrepo-com.svg";
-import clock from "../assets/images/whyus/clock-two-svgrepo-com.svg";
-import incident from "../assets/images/whyus/injury-2-svgrepo-com.svg";
-import trip from "../assets/images/whyus/travel-suitcase-3-svgrepo-com.svg";
-import tick from "../assets/images/whyus/tick-svgrepo-com.svg";
+import credit from "../assets/images/whyUs/credit-card-pay-visa-funds-svgrepo-com.svg";
+import cards from "../assets/images/whyUs/papers-stack-svgrepo-com.svg";
+import man from "../assets/images/whyUs/man-talking-avatar-svgrepo-com.svg";
+import clock from "../assets/images/whyUs/clock-two-svgrepo-com.svg";
+import incident from "../assets/images/whyUs/injury-2-svgrepo-com.svg";
+import trip from "../assets/images/whyUs/travel-suitcase-3-svgrepo-com.svg";
+import tick from "../assets/images/whyUs/tick-svgrepo-com.svg";
 
 const headerText: string = "Simple and easy!";
 
